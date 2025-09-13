e8d98aad7ea504d88dcbff76371f9fffd491a1bc
MrKaminK11 <118268784+KhalidYT11@users.noreply.github.com>
2025-09-13 17:01:04 +0300

    Add files via upload

---
+++ winimage-10.00-keygen/Form1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        usertxtbox = New TextBox()
+        Label1 = New Label()
+        GenerateButton = New Button()
+        OutputTextBox = New RichTextBox()
+        Label2 = New Label()
+        LinkLabel1 = New LinkLabel()
+        LinkLabel2 = New LinkLabel()
+        Label3 = New Label()
+        SuspendLayout()
+        ' 
+        ' usertxtbox
+        ' 
+        usertxtbox.Location = New Point(45, 49)
+        usertxtbox.Name = "usertxtbox"
+        usertxtbox.Size = New Size(289, 27)
+        usertxtbox.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(45, 26)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(49, 20)
+        Label1.TabIndex = 1
+        Label1.Text = "Name"
+        ' 
+        ' GenerateButton
+        ' 
+        GenerateButton.Location = New Point(126, 82)
+        GenerateButton.Name = "GenerateButton"
+        GenerateButton.Size = New Size(113, 29)
+        GenerateButton.TabIndex = 2
+        GenerateButton.Text = "Generate"
+        GenerateButton.UseVisualStyleBackColor = True
+        ' 
+        ' OutputTextBox
+        ' 
+        OutputTextBox.Location = New Point(12, 113)
+        OutputTextBox.Name = "OutputTextBox"
+        OutputTextBox.Size = New Size(357, 330)
+        OutputTextBox.TabIndex = 3
+        OutputTextBox.Text = ""
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(12, 446)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(156, 20)
+        Label2.TabIndex = 4
+        Label2.Text = "Made by MrKaminK11"
+        ' 
+        ' LinkLabel1
+        ' 
+        LinkLabel1.AutoSize = True
+        LinkLabel1.Location = New Point(174, 446)
+        LinkLabel1.Name = "LinkLabel1"
+        LinkLabel1.Size = New Size(56, 20)
+        LinkLabel1.TabIndex = 5
+        LinkLabel1.TabStop = True
+        LinkLabel1.Text = "GitHub"
+        ' 
+        ' LinkLabel2
+        ' 
+        LinkLabel2.AutoSize = True
+        LinkLabel2.Location = New Point(236, 446)
+        LinkLabel2.Name = "LinkLabel2"
+        LinkLabel2.Size = New Size(66, 20)
+        LinkLabel2.TabIndex = 5
+        LinkLabel2.TabStop = True
+        LinkLabel2.Text = "YouTube"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(12, 478)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(165, 20)
+        Label3.TabIndex = 6
+        Label3.Text = "Discord: kalhlaly958241"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(381, 516)
+        Controls.Add(Label3)
+        Controls.Add(LinkLabel2)
+        Controls.Add(LinkLabel1)
+        Controls.Add(Label2)
+        Controls.Add(OutputTextBox)
+        Controls.Add(GenerateButton)
+        Controls.Add(Label1)
+        Controls.Add(usertxtbox)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        Name = "Form1"
+        ShowIcon = False
+        Text = "WinImage 10.0 keygen"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents usertxtbox As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GenerateButton As Button
+    Friend WithEvents OutputTextBox As RichTextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents LinkLabel2 As LinkLabel
+    Friend WithEvents Label3 As Label
+
+End Class
---
+++ winimage-10.00-keygen/Form1.vb
@@ -0,0 +1,85 @@
+﻿Public Class Form1
+    ' The master keys from the original reverse-engineered algorithm.
+    Private ReadOnly professionals_code As ULong() = {&H10051981, &H4011995, &H2061997, &H12091999, &H16062004, &H21042002, &H13062004, &H9112005, &H24112005}
+
+    ' Helper function to shift 8 and B characters in the generated code.
+    Private Function Shift8b(ByVal input As String) As String
+        Dim result As New System.Text.StringBuilder()
+        For Each c As Char In input
+            Select Case Char.ToUpper(c)
+                Case "8"c
+                    result.Append("B"c)
+                Case "B"c
+                    result.Append("8"c)
+                Case Else
+                    result.Append(c)
+            End Select
+        Next
+        Return result.ToString()
+    End Function
+
+    ' The main function to get the registration codes.
+    Private Function GetCode(ByVal name As String) As String()
+        If String.IsNullOrWhiteSpace(name) Then
+            Return New String() {}
+        End If
+
+        Dim codes As New System.Collections.Generic.List(Of String)()
+        Dim length As Integer = name.Length
+        Dim v As Integer = length
+        Dim code As ULong = &H47694C
+
+        name = name.ToUpper()
+
+        For i As Integer = 0 To length - 1
+            If (i Mod 14) = 0 Then
+                v = 39
+            End If
+            code += CULng(v * Asc(name(i)))
+            v *= IIf((i + 3) Mod 14, 3, 7)
+        Next
+
+        For Each profCode As ULong In professionals_code
+            codes.Add(Shift8b(Hex(code + profCode).ToUpper()))
+        Next
+
+        Return codes.ToArray()
+    End Function
+
+    ' Event handler for the Generate button click.
+    Private Sub GenerateButton_Click(sender As Object, e As EventArgs) Handles GenerateButton.Click
+        ' Clear the output textbox before generating new keys.
+        OutputTextBox.Text = ""
+
+        Dim keys As String() = GetCode(usertxtbox.Text)
+        If keys.Length > 0 Then
+            For Each key As String In keys
+                OutputTextBox.AppendText(key & Environment.NewLine)
+            Next
+        End If
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Try
+            ' Navigate to the specified URL when the link is clicked.
+            System.Diagnostics.Process.Start("https://github.com/KhalidYT11")
+        Catch ex As Exception
+            ' Show an error message if the process can't be started.
+            MessageBox.Show("Could not open the link. Please visit https://github.com/KhalidYT11 manually.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        Try
+            ' Navigate to the specified URL when the link is clicked.
+            System.Diagnostics.Process.Start("https://www.youtube.com/@MrKaminK11")
+        Catch ex As Exception
+            ' Show an error message if the process can't be started.
+            MessageBox.Show("Could not open the link. Please visit https://www.youtube.com/@MrKaminK11 manually.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+        End Try
+    End Sub
+
+
+End Class
